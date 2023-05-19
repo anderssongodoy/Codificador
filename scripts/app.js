118602e6add354e40a5ae39ce5273de4f4ffe1b9
anderssongodoy <anderssongodoygarica@gmail.com>
2023-05-18 23:33:50 -0500

    Terminando Detalles

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
-
+// DEFINIR VARIABLES DEL HTML A JAVASCRIPT
 const textcontainer = document.querySelector('.text-container');
 const resultImg = document.querySelector(".area-result img");
 const resultTitle = document.getElementById("title");
 const resultText = document.getElementById("result-text");
 
+// FUNCION PARA ENCRIPTAR
 const encryptText = (text) => {
     let encryptedText = text.replace(/e/g, "enter");
     encryptedText = encryptedText.replace(/i/g, "imes");
@@ -13,6 +14,7 @@ const encryptText = (text) => {
     return encryptedText;
 }
 
+// FUNCION PARA DESENCRIPTAR
 const decryptText = (text) => {
     let decryptedText = text.replace(/enter/g, "e");
     decryptedText = decryptedText.replace(/imes/g, "i");
@@ -22,9 +24,11 @@ const decryptText = (text) => {
     return decryptedText;
 }
 
+// DEFINIR VARIABLES PARA EL RESULTADO
 const inputTextarea = document.querySelector(".first-textarea");
 const resultArea = document.querySelector(".area-result .text-container");
 
+// AÑADIR FUNCION AL BOTON ENCRIPTAR
 document.querySelector("#encrypt-btn").addEventListener("click", () => {
     const inputText = inputTextarea.value.toLowerCase();
     const encryptedText = encryptText(inputText);
@@ -42,6 +46,7 @@ document.querySelector("#encrypt-btn").addEventListener("click", () => {
     resultArea.style.display = "block";
 });
 
+// AÑADIR FUNCION AL BOTON DESCENCRIPTAR
 document.querySelector("#decrypt-btn").addEventListener("click", () => {
     const inputText = inputTextarea.value.toLowerCase();
     const decryptedText = decryptText(inputText);
@@ -59,12 +64,13 @@ document.querySelector("#decrypt-btn").addEventListener("click", () => {
     resultArea.style.display = "block";
 });
 
-
+// DEFINIR EL BOTON PARA COPIAR
 const copyBtn = document.getElementById('copy-btn');
 
+// AÑADIR FUNCION AL BOTON
 copyBtn.addEventListener('click', () => {
-    const resultArea = document.getElementById('result');
-    const resultText = resultArea.textContent;
-    navigator.clipboard.writeText(resultText);
+    const resultArea1 = document.querySelector(".area-result .text-container .text");
+    const resultText1 = resultArea1.textContent;
+    navigator.clipboard.writeText(resultText1);
     swal("Texto copiado al portapapeles");
 });
